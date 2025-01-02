200b2c193ba9f8dac7e84f620b599cdbc78c6939
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-01-02 19:43:03 +0530

    02-01-2025

---
+++ Dom.js
@@ -0,0 +1,14 @@
+// homework question 
+// what is hoisting
+// what is ES6 AND ECMA
+
+// return single node 
+let head = document.getElementById("head")
+console.log(head);
+
+document.write(" <i> Hello Vikash Singh </i>")
+// return nodelist in array format same as document.getElementsByTagName
+let head2 = document.getElementsByClassName("content2")
+console.log(head2); // it does not change any text
+head2[1].innerHTML = "<i> Vikash Singh  </i>"
+head2[2].textContent = " hey Balraj gupta "
