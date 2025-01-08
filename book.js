da21c1c923bb8ba066dda176d346ddc3aa159a0d
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-01-08 18:12:57 +0530

    07-01-25

---
+++ book.js
@@ -0,0 +1,17 @@
+function low(){
+    let con1 = document.getElementById('con1')
+     let a = parseInt(prompt("who much set do you want? "))
+     let final = document.getElementsByClassName('final')
+     final.textcontant ='  a*180';
+     final.innerHTML = "your total amout "
+}
+
+
+function high(){
+    let con1 = document.getElementById('con2')
+     let a = parseInt(prompt("who much set do you want? "))
+     let final = document.getElementsByClassName('final')
+       final.textcontant = 'a*280';
+      final.innerHTML = "your total amout "
+     
+}
