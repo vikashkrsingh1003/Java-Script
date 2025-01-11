6ce53279dfd729eb3d0b8eb61389502fc36ef3c0
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-01-11 19:24:56 +0530

    11-01-2025

---
+++ QueryS.js
@@ -0,0 +1,21 @@
+function color(){
+    let cont = document.getElementsByClassName('container')
+    console.log(cont);
+    cont[0].classList.toggle('colorr')
+}
+
+
+// querySelector
+// querySelector all
+
+let b = document.querySelector('.container')
+b.style.color = "blue"
+
+// let c = document.querySelectorAll('.container')
+// console.log(c);
+// c[1].style.color = "blue"
+
+let btn = document.querySelector('#btn')
+btn.addEventListener('click' , function(){
+    alert("Add event fire")
+})
