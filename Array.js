ca6ed0f5428cfbe05490e6d869126d4014e51ba0
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-01-23 19:01:36 +0530

    23-01-25

---
+++ Array.js
@@ -0,0 +1,34 @@
+//let arr = [1,"vikash", 23 , true,24.67]
+// console.log(arr);
+// console.log(arr[4]);
+
+//   for(let a=0; a<5 ; a++){
+
+//        console.log(arr[a]);  
+//   }
+
+//    for( let b of arr){
+//     console.log(b);
+//    } 
+
+// let ar = []
+
+// for (let a = 0; a < 5; a++) {
+
+//     ar[a] = prompt("Enter any string ")
+
+// }
+// console.log(ar)
+
+//WAp to print odd number btween 25 to 1 in array?
+ 
+let odd = []
+
+  for(let i=25 ; i>0; i--){
+     if(i%2!=0){
+      
+      console.log(odd[d])
+  
+     }
+  }
+ 
