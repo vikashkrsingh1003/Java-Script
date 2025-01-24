1064aa87b15b67904af9c88980be278426490571
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-01-24 18:52:46 +0530

    24-01-25

--- Array.js
+++ Array.js
@@ -44,20 +44,32 @@
 
 
 //push or pop function 
-let vl = ['vikash ', 12,67, 'cybrom']
+// let vl = ['vikash ', 12,67, 'cybrom']
 
-vl.push("mohit","manish")
-console.log(vl);
+// vl.push("mohit","manish")
+// console.log(vl);
 
- vl.pop()
- console.log(vl)
+//  vl.pop()
+//  console.log(vl)
 
- vl.unshift("Chashma","lagao")
- console.log(vl);
+//  vl.unshift("Chashma","lagao")
+//  console.log(vl);
 
- vl.shift()
- console.log(vl);
+//  vl.shift()
+//  console.log(vl);
 
  //slice function 
-  
- 
+ let vl1 = ['vikash ', 12, "Singh ji ",67, 'cybrom']
+
+//  let peice = vl1.slice(1,3);
+//  console.log(peice);
+
+//   // splice function 
+//   //delete at middle 
+//   vl1.splice(3,1)
+//   console.log(vl1);
+
+  //adding in middle 
+  vl1.splice(3,3,"indore")
+  console.log(vl1);
+
