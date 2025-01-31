71eb4273d54a68355f2e57ebb77ebf790cca75f8
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-01-31 18:51:43 +0530

    31 jan 2025

--- ob1.js
+++ ob1.js
@@ -1,18 +1,55 @@
-let person_student = {
+// let person_student = {
 
-    name : "vikash Singh",
-    age : "20 ",
-    contact : "8789841693",
+//     name : "vikash Singh",
+//     age : "20 ",
+//     contact : "8789841693",
 
-    account_state: {
-        account_balance : 150000,
-        account_no: 56466,
+//     account_state: {
+//         account_balance : 150000,
+//         account_no: 56466,
     
-    },
+//     },
 
-    persona_detail: {
+//     persona_detail: {
 
-        dob : "10june 2003"
-    }
+//         dob : "10june 2003"
+//     },
+
+//     document : [3434 , "ksnpk7849j"],
+
+//     fun:function(name) {
+//         alert("inside object " + name)
+//     }
+// }
+// console.log(person_student);
+// console.log(person_student.document[1]);
+
+// person_student.fun("vikash");
+
+
+
+// ==================================================
+
+// let insaan = {
+
+//     name: "balraj",
+//     age : 23,
+//     city: "munger",
+
+//     fun:function(){
+//         console.log(this.name);
+//     }
+// }
+
+// insaan.fun();
+
+let ob = {
+
+    name: "vikash",
+    age:20,
+    contact : 8789
+
 }
-console.log(person_student);
+for(let ky in ob){
+    console.log(ky);
+}
