b15fac804c8a1b5dbb2c27deabc3e4ccc7b7688e
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-01-31 18:27:04 +0530

    31 jan 2025

---
+++ ob1.js
@@ -0,0 +1,18 @@
+let person_student = {
+
+    name : "vikash Singh",
+    age : "20 ",
+    contact : "8789841693",
+
+    account_state: {
+        account_balance : 150000,
+        account_no: 56466,
+    
+    },
+
+    persona_detail: {
+
+        dob : "10june 2003"
+    }
+}
+console.log(person_student);
