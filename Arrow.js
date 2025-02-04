3c92402f87cd73a5a2b0ec8b8c866fa86f8f4975
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-02-04 18:56:24 +0530

    3 feb 2025

--- Arrow.js
+++ Arrow.js
@@ -1,7 +1,9 @@
 //Arrow function 
 
-  let fun = () => {return alert ("hello inside arrow")}
-  fun();
+//   let fun = () => {return alert ("hello inside arrow")}
+//   fun();
 
-  
- 
+
+ let add = (a,b) => {return a+b}
+
+ console.log(add(12,3)) 
