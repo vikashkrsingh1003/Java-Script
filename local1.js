09dd73843bca3c04d653bc729f22f68cba45399b
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-02-10 19:00:30 +0530

    10 feb 2025

--- local1.js
+++ local1.js
@@ -11,3 +11,22 @@ const multi = () => {
  localStorage.setItem("username" , JSON.stringify(username))
 
 }
+
+// retrive data from local stroge 
+
+let data = JSON.parse(localStorage.getItem('username'));
+console.log(data);
+
+function loginform() {
+
+    let logindata = {
+        loginemail : document.querySelector('#loginemail').value,
+        loginPassword  : document.querySelector('#loginpass').value
+    }
+
+    if(data.email != logindata.loginemail || data.password != logindata.loginPassword){
+
+        alert("user not found")
+        return false;
+    }
+}
