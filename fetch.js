a36eaefccd204618c3146dafefa5d02c32a0054d
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-02-12 18:40:04 +0530

    12 feb 2025

--- fetch.js
+++ fetch.js
@@ -13,17 +13,37 @@
 //   java script -> synchronus , lightweight ; 
 
 
- async function Demo(){
+//  async function Demo(){
 
-    let apidata = await fetch("https://jsonplaceholder.typicode.com/comments")
-     let fdata = await apidata.json();
+//     let apidata = await fetch("https://jsonplaceholder.typicode.com/comments")
+//      let fdata = await apidata.json();
      
-     let api = document.querySelector('#apidata')
-     let dt = fdata.map((e)=> {return e.name + e.id})
+//      let api = document.querySelector('#apidata')
+//      let dt = fdata.map((e)=> {return e.name + e.id})
 
-     api.innerHTML = dt;
-     //console.log(fdata);
-     
+//      api.innerHTML = dt;
+//     // console.log(fdata);
+      
+// }
+// Demo();
 
-}
-Demo();
+
+  async function datafetching (){ 
+
+   let data = await fetch("https://jsonplaceholder.typicode.com/comments")
+   let resdata = await data.json();
+
+   let fdata = resdata.map((e) => `
+   <tr> 
+       <td> ${e.id}</td>
+       <td> ${e.title}</td>
+       <td> ${e.body}</td>
+
+       </tr>
+   `).join("")
+
+   document.querySelector('#showdata').innerHTML = fdata
+
+  }
+
+  datafetching();
